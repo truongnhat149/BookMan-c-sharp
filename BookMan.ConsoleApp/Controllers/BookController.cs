20768ff90de90084bdf6c0018cd37ea594c5a12b
Truong <vjc.ichi.149@gmail.com>
2022-06-25 18:36:34 +0700

    book controller

---
+++ BookMan.ConsoleApp/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BookMan.ConsoleApp.Controllers
+{
+    using Models; //lưu ý cách dùng using với không gian tên con
+    using Views;
+    /// <summary>
+    /// lớp điều khiển, giúp ghép nối dữ liệu sách với giao diện
+    /// </summary>
+    internal class BookController
+    {
+        /// <summary>
+        /// ghép nối dữ liệu 1 cuốn sách với giao diện hiển thị 1 cuốn sách
+        /// </summary>
+        /// <param name="id">mã định danh của cuốn sách</param>
+        public void Single(int id)
+        {
+            // khởi tạo object với property
+            Book model = new Book
+            {
+                Id = 1,
+                Authors = "Adam Freeman",
+                Title = "Expert ASP.NET Web API 2 for MVC Developers (The Expert's Voice in .NET)",
+                Publisher = "Apress",
+                Year = 2014,
+                Tags = "c#, asp.net, mvc",
+                Description = "Expert insight and understanding of how to create, customize, and deploy complex, flexible, and robust HTTP web services",
+                Rating = 5,
+                Reading = true
+            };
+            // khởi tạo view
+            BookSingleView view = new BookSingleView(model);
+            // gọi phương thức Render để thực sự hiển thị ra màn hình
+            view.Render();
+        }
+
+    }
+}
--- BookMan.ConsoleApp/Program.cs
+++ BookMan.ConsoleApp/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace BookMan.ConsoleApp
 {
+    using Controllers;
     internal class Program
     {
         static void Main(string[] args)
         {
+            BookController controller = new BookController();
+            controller.Single(0);
+            Console.ReadKey();
         }
     }
 }
